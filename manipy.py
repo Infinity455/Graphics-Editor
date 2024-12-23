30011709f71ebc93b49542cdd5b8bb030c24542d
Infinity455 <er.zayasramos@ufl.edu>
2024-12-22 21:50:54 -0500

    Update manipy.py
    Added menu bar and reworked canvas integration

--- manipy.py
+++ manipy.py
@@ -1,18 +1,37 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QWidget, QLabel, QGridLayout, QPushButton, QVBoxLayout
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QWidget, QLabel, QGridLayout, QPushButton, QVBoxLayout, QBoxLayout, QAction
 from PyQt5.QtGui import QColor, QPainter, QPen, QPixmap
 
 class Canvas(QWidget):
-    def __init__(self, width, height):
-        super().__init__()
-        self.resize(width, height)
+    def __init__(self, parent=None, width=1000, height=750):
+        super().__init__(parent)
+        self.setSize(width, height)
+        self.initOrigin = (460, 160)
+        self.move(self.initOrigin[0], self.initOrigin[1])
 
         self.pixmap = QPixmap(width, height)
         self.pixmap.fill(QColor("white"))
+
+    def addLayer():
+        pass
+
+    def removeLayer():
+        pass
+
+    def setSize(self, width, height):
+        self.resize(width, height)
         
+    def getEdges(self):
+        # returns a tuple of QPoints of each edge of the canvas
+        return (self.geometry().bottomLeft(), self.geometry().bottomRight(), self.geometry().topLeft(), self.geometry().topRight())
+    
+    def getSides(self):
+        # returns a tuple of the sides (xLeft, yTop, xRight, yBottom)
+        return self.geometry().getRect()
+
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.drawPixmap(460, 160, self.pixmap)
+        painter.drawPixmap(self.initOrigin[0], self.initOrigin[1], self.pixmap)
 
 class ToolBox(QWidget):
     def __init__(self, parent=None):
@@ -41,6 +60,9 @@ class ToolBox(QWidget):
         self.fillButton.clicked.connect(self.fillClicked)
         self.masterLayout.addWidget(self.fillButton, 2, 0)
 
+        self.setWindowFlags(self.windowFlags() | Qt.Tool)
+        self.show()
+
     def pencilClicked(self):
         pass
     def brushClicked(self):
@@ -61,28 +83,30 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set up of overall initial GUI
+        # Set up basics of 
+        self.setUpMenuBar()
         self.setStyleSheet("background-color: grey;")
 
-        self.menu = QMenuBar()
+        # Creating Canvas with default size
+        self.canvas = Canvas(self)
+        self.canvas.setMouseTracking(True)
+        self.setCentralWidget(self.canvas)
+        
+        # toolbar window for mouse tracking in main window
+        self.setMouseTracking(True)
+
         self.locationLabel = QLabel(self)
         self.locationLabel.setText("0, 0")
         self.locationLabel.setWindowFlags(self.locationLabel.windowFlags() | Qt.Tool)
         self.locationLabel.setGeometry(10, 200, 100, 30)
         self.locationLabel.show()
 
+        # side layout to initialize locationLabel at
         self.sideLayout = QVBoxLayout()
         self.sideLayout.addWidget(self.locationLabel)
 
-        self.canvas = Canvas(1000, 750)
-        self.canvas.setMouseTracking(True)
-        self.setCentralWidget(self.canvas)
-
+        # toolbar window for tools for painting
         self.toolWindow = ToolBox(self)
-        self.toolWindow.setWindowFlags(self.toolWindow.windowFlags() | Qt.Tool)
-        self.toolWindow.show()
-
-        self.setMouseTracking(True)
 
         # Set up of brushes and tools
         self.drawing = False
@@ -95,6 +119,23 @@ class Window(QMainWindow):
         y = position.y()
 
         self.locationLabel.setText(f"{x}, {y}")
+
+    def setUpMenuBar(self):
+        self.menuBar = QMenuBar()
+        self.menuBar.setStyleSheet("background-color: white;")
+
+        fileMenu = QMenu("File", self)
+        newAction = QAction("New", self)
+        openAction = QAction("Open", self)
+        saveAction = QAction("Save", self)
+        saveAsAction = QAction("Save As", self)
+        fileMenu.addAction(newAction)
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(saveAsAction)
+
+        self.menuBar.addMenu(fileMenu)
+        self.setMenuBar(self.menuBar)
 
 if __name__ == "__main__":
     app = QApplication([])
