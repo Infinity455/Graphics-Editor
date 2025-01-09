049e26fcab825e555c8a38f8e82d2103e2648a4c
Infinity455 <er.zayasramos@ufl.edu>
2025-01-08 22:03:59 -0500

    Improved Dynamic Color Sliders
    Sliders now change to their respective positions based on relevant changes of other sliders or of the graphic gradients.

--- src/colorPicker.py
+++ src/colorPicker.py
@@ -14,7 +14,8 @@ class ColorPicker(QWidget):
         self.colorPick = False
 
         self.brush = Brush()
-        self.__color = QColor(255, 255, 255)
+        self.__color = QColor()
+        self.__color.setHsv(180, 0, 0)
 
         self.boxLabel = QLabel(self)
         self.createBoxGradient(200, QColor("cyan"))
@@ -53,23 +54,25 @@ class ColorPicker(QWidget):
 
         sliderList = ["HSV", "RGB", "CMYK", "HSL"]
 
-        
+        self.allSliders = dict()
         for repType in sliderList:
             mainContainer = QWidget(self)
             mainLayout = QVBoxLayout(self)
+            tempSlides = list()
             for char in repType:
                 tempContainer = QWidget()
                 rowLayout = QHBoxLayout()
                 name = QLabel(char)
                 slider = QSlider(Qt.Horizontal, tempContainer)
                 slider.setRange(0, 360 if char == "H" else 255)
-                slider.setValue(180)
                 valueLabel = QLabel("128", tempContainer)
                 valueLabel.setFixedWidth(40)
                 slider.valueChanged.connect(
                     lambda value, label=valueLabel, context=repType, currentRep=char: 
                     self.sliderTasks(context, currentRep, value, label) 
                 )
+
+                tempSlides.append((char, slider, valueLabel))
 
                 tempContainer.setMinimumHeight(40)
 
@@ -80,9 +83,11 @@ class ColorPicker(QWidget):
                 tempContainer.setLayout(rowLayout)
                 mainLayout.addWidget(tempContainer)
 
+            self.allSliders.update({repType: tempSlides})
             mainContainer.setLayout(mainLayout)
             self.repLayouts.addWidget(mainContainer)
 
+        self.setAllSlides()
 
         containerForStack = QWidget(self)
         containerForStack.setLayout(self.repLayouts)
@@ -100,45 +105,44 @@ class ColorPicker(QWidget):
         match(context):
             case "HSV":
                 hsv = self.__color.getHsv() #(h, s, v, alpha)
-                if currentRep is "H":
+                if currentRep == "H":
                     self.__color.setHsv(value, hsv[1], hsv[2], hsv[3])
-                    tempColor = QColor(0, 0, 0)
-                    tempColor.setHsv(value, 255, 255)
-                    self.createBoxGradient(200, tempColor)
-                elif currentRep is "S":
+                elif currentRep == "S":
                     self.__color.setHsv(hsv[0], value, hsv[2], hsv[3])
                 else:
                     self.__color.setHsv(hsv[0], hsv[1], value, hsv[3])
             case "RGB":
                 rgb = self.__color.getRgb()
-                if currentRep is "R":
+                if currentRep == "R":
                     self.__color.setRgb(value, rgb[1], rgb[2], rgb[3])
-                elif currentRep is "G":
+                elif currentRep == "G":
                     self.__color.setRgb(rgb[0], value, rgb[2], rgb[3])
                 else:
                     self.__color.setRgb(rgb[0], rgb[1], value, rgb[3])
             case "CMYK":
                 cymk = self.__color.getCmyk()
-                if currentRep is "C":
+                if currentRep == "C":
                     self.__color.setCmyk(value, cymk[1], cymk[2], cymk[3], cymk[4])
-                elif currentRep is "M":
+                elif currentRep == "M":
                     self.__color.setCmyk(cymk[0], value, cymk[2], cymk[3], cymk[4])
-                elif currentRep is "Y":
+                elif currentRep == "Y":
                     self.__color.setCmyk(cymk[0], cymk[1], value, cymk[3], cymk[4])
                 else:
                     self.__color.setCmyk(cymk[0], cymk[1], cymk[2], value, cymk[4])
             case "HSL":
                 hsl = self.__color.getHsl() #(h, s, l, alpha)
-                if currentRep is "H":
-                    self.__color.setHsv(value, hsl[1], hsl[2], hsl[3])
-                    tempColor = QColor(0, 0, 0)
-                    tempColor.setHsl(value, 255, 255)
-                    self.createBoxGradient(200, tempColor)
-                elif currentRep is "S":
-                    self.__color.setHsv(hsl[0], value, hsl[2], hsl[3])
+                if currentRep == "H":
+                    self.__color.setHsl(value, hsl[1], hsl[2], hsl[3])
+                elif currentRep == "S":
+                    self.__color.setHsl(hsl[0], value, hsl[2], hsl[3])
                 else:
                     self.__color.setHsl(hsl[0], hsl[1], value, hsl[3])
+        tempColor = self.__color.toHsv()
+        tempColor.setHsv(tempColor.hue(), 255, 255)
+        # print(f"{context}, {currentRep}, {tempColor.hue()}")
+        self.createBoxGradient(200, tempColor)
         self.changePrimaryColor()
+        self.setAllSlides()
         
 
     def sliderStyleSheets(self, context, currentRep):
@@ -146,9 +150,9 @@ class ColorPicker(QWidget):
         
         match(context):
             case "HSV":
-                if currentRep is "H":
+                if currentRep == "H":
                     pass
-                elif currentRep is "S":
+                elif currentRep == "S":
                     pass
                 else:
                     pass
@@ -160,17 +164,64 @@ class ColorPicker(QWidget):
                 pass
 
     def createCircleIndicator(self):
-        pixmap = QPixmap(10, 10)
+        size = 10
+
+        pixmap = QPixmap(size, size)
         pixmap.fill(Qt.transparent)
 
         with QPainter(pixmap) as painter:
             painter.setBrush(Qt.transparent)
             painter.setPen(Qt.black)
-            painter.drawEllipse(0, 0, 9, 9)
+            painter.drawEllipse(0, 0, size - 1, size - 1)
 
         return pixmap
+    
+    def setAllSlides(self):
+        for key in self.allSliders.keys():
+            for char, slider, label in self.allSliders[key]:
+                slider.blockSignals(True)
+                match(key):
+                    case "HSV":
+                        hsv = self.__color.getHsv()
+                        if char == "H":
+                            val = self.__color.hue()
+                        elif char == "S":
+                            val = hsv[1]
+                        else:
+                            val = hsv[2]
+                    case "RGB":
+                        rgb = self.__color.getRgb()
+                        if char == "R":
+                            val = rgb[0]
+                        elif char == "G":
+                            val = rgb[1]
+                        else:
+                            val = rgb[2]
+                    case "CMYK":
+                        cmyk = self.__color.getCmyk()
+                        if char == "C":
+                            val = cmyk[0]
+                        elif char == "M":
+                            val = cmyk[1]
+                        elif char == "Y":
+                            val = cmyk[2]
+                        else:
+                            val = cmyk[3]
+                    case "HSL":
+                        hsl = self.__color.getHsl()
+                        if char == "H":
+                            val = self.__color.hue()
+                        elif char == "S":
+                            val = hsl[1]
+                        else:
+                            val = hsl[2]
+                slider.setValue(val)
+                label.setText(str(val))
+                slider.blockSignals(False)
 
-    def createBoxGradient(self, side, hue):
+    def createBoxGradient(self, side, hue: QColor):
+        self.hueColor = hue
+
         pixmap = QPixmap(side, side)
         pixmap.fill(Qt.transparent)
 
@@ -229,22 +280,35 @@ class ColorPicker(QWidget):
         if self.boxLabel.geometry().contains(event.pos()):
             boxPos = self.boxLabel.mapFromGlobal(event.globalPos())
             self.__color = self.boxImage.pixelColor(boxPos.x(), boxPos.y())
+            tempColor = self.__color.toHsv()
+            tempColor.setHsv(tempColor.hue(), 255, 255)
+            self.createBoxGradient(200, tempColor)
             self.changePrimaryColor()
+            self.setAllSlides()
             self.circleIndicatorLabel.move(boxPos.x(), boxPos.y())
         elif self.spectrumLabel.geometry().contains(event.pos()):
             specPos = self.spectrumLabel.mapFromGlobal(event.globalPos())
             color = self.specImage.pixelColor(specPos.x(), specPos.y())
-            self.changePrimaryColor()
             self.createBoxGradient(200, color)
+            self.changePrimaryColor()
+            self.setAllSlides()
 
     def mouseMoveEvent(self, event):
         if self.colorPick:
-            if self.spectrumLabel.geometry().contains(event.pos()):
+            if self.boxLabel.geometry().contains(event.pos()):
+                boxPos = self.boxLabel.mapFromGlobal(event.globalPos())
+                self.__color = self.boxImage.pixelColor(boxPos.x(), boxPos.y())
+                tempColor = self.__color.toHsv()
+                tempColor.setHsv(tempColor.hue(), 255, 255)
+                self.createBoxGradient(200, tempColor)
+                self.changePrimaryColor()
+                self.setAllSlides()
+                self.circleIndicatorLabel.move(boxPos.x(), boxPos.y())
+            elif self.spectrumLabel.geometry().contains(event.pos()):
                 specPos = self.spectrumLabel.mapFromGlobal(event.globalPos())
                 color = self.specImage.pixelColor(specPos.x(), specPos.y())
                 self.createBoxGradient(200, color)
-            elif self.boxLabel.geometry().contains(event.pos()):
-                boxPos = self.boxLabel.mapFromGlobal(event.globalPos())
-                self.__color = self.boxImage.pixelColor(boxPos.x(), boxPos.y())
                 self.changePrimaryColor()
-                self.circleIndicatorLabel.move(boxPos.x(), boxPos.y())
+                temp = self.__color.toHsv().getHsv()
+                self.__color.setHsv(color.toHsl().hue(), temp[1], temp[2])
+                self.setAllSlides()
